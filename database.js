cdc79c46fc899318ae436b0545d15466cc2889a5
Alexandre Xavier <alxavier@Mac-mini-de-Alexandre.local>
2021-07-13 23:54:11 -0300

    Primeiro commit

---
+++ database.js
@@ -0,0 +1,98 @@
+const bookstore = [
+    {id:1,
+        name:"Design Data-Intense Applications",
+        author: "Martin Kleppmann",
+        category:"Data Engineering" ,
+        pages: 600,
+        recommends: true,
+        read: true
+    },
+    {id:2,
+        name:"Pratical Hive: A Guide to Hadoop’s Data Warehouse System",
+        author: "Scott Shaw",
+        category: "Data analysis",
+        pages: 200,
+        recommends: true,
+        read: false
+    
+    },
+    {id:3,
+        name: "Foundations for Architecting Data Solutions",
+        author: "Ankur Gupla",
+        category: "Data analysis",
+        pages: 200,
+        recommends: true,
+        read: false   
+    },    
+    {id:4,
+        name:"Data Lake for Enterprise",
+        author: "Ted Malaska & Jonathan Seidman",
+        category: "Data",
+        pages: 600,
+        recommends: true,
+        read: true
+    
+    },
+
+    {id:5,
+        name:"Pratical Enterprise Data Lake Insights",
+        author: "Saurabh Gupta",
+        category: "Data analysis",
+        pages: 300,
+        recommends: false,
+        read: false
+    
+    },
+
+    {id:6,
+        name:"Mãos à Obra: Aprendizado de Máquina com Scikit-Learn & TensorFlow",
+        author: "Aurélien Géron",
+        category: "Developer" ,
+        pages: 150,
+        recommends: true,
+        read: true
+
+    },
+    {id:7,
+        name:"Algoritmos e Estruturas de Dados",
+        author: "Guimaraes,Lages",
+        category: "Algoritmos",
+        pages: 280,
+        recommends: true,
+        read: true 
+
+    },
+    {
+    id:8,
+    name:"Java® Como Programar",
+        author: "Paul Deitel",
+        category: "Programacao",
+        pages: 400,
+        recommends: true,
+        read: false
+
+    },
+    {
+    id:9,
+
+    name:"Javascript e Jquery: desenvolvimento de interfaces web interativas ",
+        author: "Jon Ducket",
+        category: "Development" ,
+        pages: 300,
+        recommends: true ,
+        read: true
+
+    },
+    {
+    id:10,
+    name:"Introdução à Programação com Python: Algoritmos e Lógica de Programação Para Iniciantes",
+        author: "Nilo Ney Coutinho Menezes" ,
+        category: "Algoritmos" ,
+        pages: 500,
+        recommends: true,
+        read: false
+
+    }
+        
+]
+module.exports = bookstore
